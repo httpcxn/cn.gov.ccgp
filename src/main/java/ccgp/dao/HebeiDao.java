2e09cd11b1d74a1a1de4401edd09eced7ad05cf5
cxn <1790217432@qq.com>
2018-08-28 09:25:45 +0800

    IDEA整合GitHub测试

---
+++ src/main/java/ccgp/dao/HebeiDao.java
@@ -0,0 +1,39 @@
+package ccgp.dao;
+
+import ccgp.domain.Ccgp;
+import ccgp.utils.JDBCUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class HebeiDao {
+    QueryRunner qr = new QueryRunner(JDBCUtils.getDataSource());
+    public Logger logger = LogManager.getLogger(HebeiDao.class.getName());
+    public void save(ArrayList<Ccgp> list) {
+        if (list!=null&&list.size()>0) {
+            for (Ccgp ccgp : list) {
+                Object[] params= {
+                        ccgp.getTitle(),
+                        ccgp.getTime(),
+                        ccgp.getArea(),
+                        ccgp.getHref(),
+                        ccgp.getStatus(),
+                };
+                String sql="INSERT INTO hebei (title,TIME,AREA,href,STATUS) VALUES (?,?,?,?,?)";
+                try {
+                    qr.insert(sql, new ScalarHandler<Object>(), params);
+                } catch (SQLException e) {
+                    if (e.getErrorCode() == 1062){
+                        logger.warn("已存在记录,链接："+ccgp.getHref());
+                    }else {
+                        logger.error(e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/ccgp/utils/JDBCUtils.java
@@ -0,0 +1,29 @@
+package ccgp.utils;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+public class JDBCUtils {
+	
+	
+	/*
+	 * �������ӳ�BasicDataSource
+	 */
+	public static BasicDataSource dataSource = new BasicDataSource();
+	//��̬�����
+	static {
+
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver"); // ����Ҫ���ӵ����ݿ������
+		dataSource.setUrl("jdbc:mysql://localhost/cxn_data?useUnicode=true&characterEncoding=utf-8&useSSL=false"); //ָ��Ҫ���ӵ����ݿ��ַ
+		dataSource.setUsername("root"); //ָ��Ҫ�������ݵ��û���
+		dataSource.setPassword(""); //ָ��Ҫ�������ݵ�����
+			}
+	/*
+	 * �������ӳض���
+	 */
+	public static DataSource getDataSource(){
+		return dataSource;
+	}
+	
+}
