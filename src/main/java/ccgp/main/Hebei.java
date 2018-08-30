4c48041459fa14296f19fc996df783f2b64f1c9f
cxn <1790217432@qq.com>
2018-08-30 16:17:54 +0800

    河北第一版

--- src/main/java/ccgp/main/Hebei.java
+++ src/main/java/ccgp/main/Hebei.java
@@ -1,8 +1,8 @@
 package ccgp.main;
 
+
 import ccgp.dao.HebeiDao;
 import ccgp.domain.Ccgp;
-import ccgp.utils.Constant;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -10,68 +10,101 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Hebei {
-    public Logger logger = LogManager.getLogger(Hebei.class.getName());
-    public void heBei(){
-        logger.debug("开始处理 河北");
-        ArrayList<Ccgp> list = new ArrayList<Ccgp>();
-        String[] urlList = Constant.hebei;
-        for (int i = 0; i < urlList.length; i++) {
-            list = analysis(urlList[i],list);
+    private String str ="";
+    private String status = "";//信息的分类
+    private String url = "";
+    private String  title="";
+    private String  time="";
+    private String  area="";
+    private String  people="";
+    private String  href="";
+    private Logger logger = LogManager.getLogger(Hebei.class.getName());
+
+
+    public void Hebei1(String oldurl){
+        logger.debug("开始处理:"+oldurl);
+        str=oldurl+"index_";
+        status = str.substring(str.length()-11, str.length()-7);
+        System.out.println(str.length()-11);
+        System.out.println(str.length()-7);
+        System.out.println(status);
+        //获取数据库中的最大时间
+        HebeiDao hb=new HebeiDao();
+        Date maxTime = hb.getTime(status);
+        logger.debug("当前数据库中最新数据时间"+maxTime.toString());
+        ArrayList<Ccgp> list = analysis(maxTime,oldurl);
+        if (list!=null && !list.isEmpty()) {
+            hb.hebei(list);
+            logger.debug("写入完成...");
         }
-        HebeiDao hebei=new HebeiDao();
-        hebei.save(list);
-        logger.debug("结束处理 河北");
+        logger.debug("结束处理:"+oldurl);
+        System.out.println("oldurl"+oldurl);
     }
-    private ArrayList<Ccgp> analysis(String url, ArrayList<Ccgp> list) {
-        int a = 5;
-        String[] urls = url.split("/");
-        String status = urls[urls.length-2];
-        Document doc=null;
-        for(int i=1;i<a;i++) {
-            String newUrl=url+i+".html";
-            try {
-                doc = Jsoup.connect(newUrl).timeout(30000).get();
-            } catch (IOException e) {
-                logger.error("河北链接失败");
-                logger.error(e.getMessage());
+    //解析网页并把信息存入集合中
+    private ArrayList<Ccgp> analysis(Date maxTime, String oldurl) {
+        ArrayList<Ccgp> list = new ArrayList<Ccgp>();
+        for (int i = 0; i < 25; i++) {
+            if (i == 0) {
+                url = oldurl + "index.html";
+            } else {
+                url = str + i + ".html";
             }
             try {
-                Elements paginationLis = doc.getElementsByClass("pagination").get(0).children();
-                String page = paginationLis.get(paginationLis.size()-1).text();
-                char c = page.charAt(3);
-                a=c-'0';
-                Elements lis = doc.getElementsByClass("news-list-content list-unstyled margin-top-20").get(0).children();
-                for (Element li : lis) {
-                    String title = li.child(0).text();
-                    String href = li.child(0).attr("abs:href");
-                    String time = li.child(1).text();
-                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-                    Date time1 = null;
-                    try {
-                        time1 = format.parse(time);
-                    } catch (ParseException e) {
-                        logger.error("河北日期转换异常");
-                        logger.error(e.getMessage());
-                    }
+                Document document = Jsoup.connect(url).timeout(30000).get();
+                Element u = document.getElementsByClass("a3").get(3);
+                Element ul = document.getElementsByClass("txt").get(0);
+                Element u2 = document.getElementsByClass("txt").get(1);
+                Element u3 = document.getElementsByClass("txt").get(2);
+                //Elements elements1 = document.select("table").select("tr");
+                try {
+                    //Elements lis = ul.children();
+                    title = u.text();
+                     time = ul.text();
+                     area = u2.text();
+                     people = u3.text();
+                     href=u.attr("abs:href");
+                    System.out.println(title+time + area + people+href);
+/*
+                    for (Element li : lis) {
+                        String title = li.child(0).text();
+                        String href = li.child(0).attr("abs:href");
+                        String time = li.child(1).text();
+                        String area = li.child(2).text();
+                        String people = li.child(3).text();
+
+                        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                        Date time1 = format.parse(time);
+                        if (time1.before(maxTime) || time1.equals(maxTime)) {
+                            return list;
+                        }
+*/
+                /*   SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                    Date time1 = format.parse(time);
+                    if (time1.before(maxTime) || time1.equals(maxTime)) {
+                        return list;
+                    }*/
+                   // SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                   // Date time1= format.parse(time);
                     Ccgp cc = new Ccgp();
-                    cc.setTitle(title);
-                    cc.setTime(time1);
-                    cc.setHref(href);
+                     cc.setTitle(title);
+                    cc.setArea(area);
+                    cc.setPeople(people);
+                    //cc.setTime(time1);
                     cc.setStatus(status);
-                    cc.setArea("河北");
+                    cc.setHref(href);
 
                     logger.info(cc.toString());
                     list.add(cc);
+
+                } catch (Exception e) {
+                    logger.error(e.getMessage());
                 }
             } catch (Exception e) {
-                logger.error("河北解析失败");
                 logger.error(e.getMessage());
             }
         }
